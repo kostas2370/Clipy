f298df2409056451fb76cffeae9ac98a2762e2a2
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2022-05-25 00:42:35 +0300

    updated

---
+++ Automated youtuber/src2/settings.py
@@ -0,0 +1,6 @@
+class settings :
+    client_id='6912jn6k27v3ntta63m6hmmikz0vgb'
+    client_secret='7b162j8k9tsaetbjk5tbyt85i4wi9g'
+    selenium_location='C:/Program Files (x86)/chromedriver.exe'
+
+
